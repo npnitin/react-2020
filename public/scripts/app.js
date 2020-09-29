22484e16932033027e39cbee045fd10b40e3dad8
Nitin Ashok Pawar <npawar@altimetrik.com>
2020-09-29 11:29:07 +0530

    changes template to work in jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 console.log('react logs');
 
-var template = <p>This is JSX from app.js</p>;
+var template = React.createElement("p", null, "This is JSX from app.js");
 var appRoot = document.getElementById('app');
 ReactDOM.render(template,appRoot);
